ee0ecd7ed68b734d0f62dc86bfa5fc2d3fd81518
Omar-Martinez-F <omartinezfuentes@csumb.edu>
2026-03-23 17:53:58 -0700

    first commit for lab

---
+++ Hello_World.py
@@ -0,0 +1,17 @@
+from flask import Flask , render_template
+from flask_bootstrap import Bootstrap5
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+@app.route("/")
+def task2():
+    p1 = "<p>Erin: lol</p>"
+    p2 = "<p>Teddy: lmao</p>"
+    p3 = "<p>Ariel: bo2 </p>"
+    return p1+p2 + p3
+
+
+
+@app.route("/omar")
+def task3():
+    return render_template("template.html")
